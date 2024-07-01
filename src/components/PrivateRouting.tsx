a868fd05aa4157540736cd23fdc46591086a25ed
arielirmawan <arielirmawan@gmail.com>
2024-07-02 02:29:13 +0700

    revisi ketiga 0.2 (private routing)

--- src/components/PrivateRouting.tsx
+++ src/components/PrivateRouting.tsx
@@ -1,8 +1,9 @@
 import { Navigate, To } from "react-router-dom";
+import { useSignInState } from "./context/SignInToken";
 // import { useAuth } from "../contexts/auth";
 
 function PrivateRoute({ children, to }: { children: JSX.Element; to: To }) {
-  const { token } = useAuth();
+  const { token } = useSignInState();
   if (!token) return <Navigate to={to} replace />;
   return children;
 }
